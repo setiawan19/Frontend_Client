da6e29957ad020c233c2798784173b3054a68b30
setiawan <setiawansocial@gmail.com>
2019-07-18 22:48:32 +0700

    updatefix

--- src/components/DataTraining.js
+++ src/components/DataTraining.js
@@ -26,163 +26,175 @@ export class DataTraining extends Component {
             <table style={{ border: "1px solid black" }}>
               <thead>
                 <tr style={{ border: "1px solid black" }}>
-                  <th style={{ border: "1px solid black" }}>Attribut</th>
-                  <th style={{ border: "1px solid black" }}>Keterangan</th>
-                  <th style={{ border: "1px solid black" }}>Variabel</th>
+                  <th style={{ border: "1px solid black" }}>IPK</th>
+                  <th style={{ border: "1px solid black" }}>IPS</th>
+                  <th style={{ border: "1px solid black" }}>SKS</th>
+                  <th style={{ border: "1px solid black" }}>Kelayakan</th>
                 </tr>
               </thead>
               <tbody style={{ border: "1px solid black" }}>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Jenis Kelamin</td>
+                  <td style={{ border: "1px solid black" }}>Tinggi</td>
+                  <td style={{ border: "1px solid black" }}>Tinggi</td>
+                  <td style={{ border: "1px solid black" }}>Penuh</td>
                   <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Pria</tr>
-                    <tr style={{ border: "1px solid black" }}>Wanita</tr>
+                    Lulus Tepat Waktu
                   </td>
-                  <td style={{ border: "1px solid black" }}>X1</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Umur</td>
+                  <td style={{ border: "1px solid black" }}>Sedang</td>
+                  <td style={{ border: "1px solid black" }}>Sedang</td>
+                  <td style={{ border: "1px solid black" }}>Penuh</td>
                   <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}> 19-24 </tr>
-                    <tr style={{ border: "1px solid black" }}>25-30 </tr>
-                    <tr style={{ border: "1px solid black" }}> 31-36 </tr>
-                    <tr style={{ border: "1px solid black" }}> 37-42 </tr>
-                    <tr style={{ border: "1px solid black" }}> 43-48 </tr>
-                    <tr style={{ border: "1px solid black" }}> 49-54 </tr>
-                    <tr style={{ border: "1px solid black" }}> 55-60 </tr>
+                    Lulus Tepat Waktu
                   </td>
-                  <td style={{ border: "1px solid black" }}>X2</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Pekerjaan</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Penuh</td>
                   <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Wiraswasta</tr>
-                    <tr style={{ border: "1px solid black" }}>
-                      Karyawan Swasta
-                    </tr>
-                    <tr style={{ border: "1px solid black" }}>PNS</tr>
+                    Lulus Tepat Waktu
                   </td>
-                  <td style={{ border: "1px solid black" }}>X3</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Penghasilan</td>
-                  <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>1-5 Juta</tr>
-                    <tr style={{ border: "1px solid black" }}>6-10 Juta</tr>
-                    <tr style={{ border: "1px solid black" }}>11-15 Juta</tr>
-                    <tr style={{ border: "1px solid black" }}>> 15 Juta</tr>
-                  </td>
-                  <td style={{ border: "1px solid black" }}>X4</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Penuh</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Lulus</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Status Rumah</td>
-                  <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Orang Tua</tr>
-                    <tr style={{ border: "1px solid black" }}>
-                      Sendiri (Cicil)
-                    </tr>
-                    <tr style={{ border: "1px solid black" }}>
-                      Sendiri (Lunas)
-                    </tr>
-                  </td>
-                  <td style={{ border: "1px solid black" }}>X5</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Sedang</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Penuh</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Lulus</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Status Marital</td>
+                  <td style={{ border: "1px solid black" }}>Sedang</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Penuh</td>
                   <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Belum Menikah</tr>
-                    <tr style={{ border: "1px solid black" }}>Menikah</tr>
+                    Lulus Tepat Waktu
                   </td>
-                  <td style={{ border: "1px solid black" }}>X6</td>
                 </tr>
                 <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Jumlah Anak</td>
-                  <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>0</tr>
-                    <tr style={{ border: "1px solid black" }}>1</tr>
-                    <tr style={{ border: "1px solid black" }}>2</tr>
-                    <tr style={{ border: "1px solid black" }}>3</tr>
-                    <tr style={{ border: "1px solid black" }}>>3</tr>
-                  </td>
-                  <td style={{ border: "1px solid black" }}>X7</td>
-                </tr>
-                <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>
-                    Metode Pembayaran
-                  </td>
-                  <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Debit Card</tr>
-                    <tr style={{ border: "1px solid black" }}>Credit Card</tr>
-                  </td>
-                  <td style={{ border: "1px solid black" }}>X8</td>
-                </tr>
-                <tr style={{ border: "1px solid black" }}>
-                  <td style={{ border: "1px solid black" }}>Kategori</td>
-                  <td style={{ border: "1px solid black" }}>
-                    <tr style={{ border: "1px solid black" }}>Member</tr>
-                    <tr style={{ border: "1px solid black" }}>Non Member</tr>
-                  </td>
-                  <td style={{ border: "1px solid black" }}>X9</td>
+                  <td style={{ border: "1px solid black" }}>Sedang</td>
+                  <td style={{ border: "1px solid black" }}>Rendah</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Penuh</td>
+                  <td style={{ border: "1px solid black" }}>Tidak Lulus</td>
                 </tr>
               </tbody>
             </table>
             <div>
               <p>
-                <b>Perhitungan Metode Naïve Bayes Pada Data Test Diatas</b>
+                <b>Perhitungan Metode C4.5</b>
               </p>
               <p>
-                P ( Y = Member ) = 7/10 = 0.7 P ( Y = Non-Member ) = 3/10 = 0.3
+                <b>Entrophy (S) :</b>
+              </p>
+              <p> ((4/7)*IMLOG2(4/7)+(-3/7)*IMLOG2(3/7))=0,985228136</p>
+              <p>
+                <b>Gain (S,A) :</b>
+              </p>
+              <p>
+                0,985228136-((1/7)*0)-((3/7)*0,918295834)-((3/7)*0,918295834) =
+                0,198117421
               </p>
             </div>
-            <div>
-              <p>
-                <b>Mencari Nilai Probabilitas Pada Tiap Atribut</b>
-              </p>
-              <p>P ( Jenis Kelamin = P | Y = Member ) = 6/7 = 0.857</p>
-              <p>P ( Umur = P| Y = Member ) = 2/7 = 0.285</p>
-              <p>P ( Pekerjaan = Swasta | Y = Member) = 2/7 = 0.285</p>
-              <p>P ( Penghasilan = 5-10 Juta | Y = Member ) = 0/7= 0</p>
-              <p>P ( Status Rumah = Orang Tua | Y = Member ) = 1/7 = 0.142</p>
-              <p>
-                P ( Status Marital = Belum Menikah | Y = Member ) = 3/7 = 0.428
-              </p>
-              <p>
-                P ( Jumlah Anak = 0 | Y = Member ) = 3/7 = 0.428 P ( Metode
-                Pembayaran = Debit Card | Y = Member ) = 3/7 = 0.428
-              </p>
-              <p>P ( Jenis Kelamin = P | Y = Non-Member ) = 2/3 = 0.666</p>
-              <p>P ( Umur = P | Y =Non-Member ) = 1/3 = 0.333</p>
-              <p>P ( Pekerjaan = Swasta | Y = Non-Member ) = 3/7 = 1</p>
-              <p>P ( Penghasilan = 5-10 Juta | Y = Non-Member ) = 3/3 = 1</p>
-              <p>
-                P ( Status Rumah = Orang Tua | Y = Non-Member ) = 1/3 = 0.333
-              </p>
-              <p>
-                P ( Status Marital = Belum Menikah | Y = Non-Member ) = 3/3 = 1
-              </p>
-              <p>P ( Jumlah Anak = 0 | Y = Non-Member) = 3/3 = 1</p>
-              <p>
-                P ( Metode Pembayaran = Debit Card | Y = Non-Member) = 3/3 = 1
-              </p>
-            </div>
-            <div>
-              <p>
-                <b> Menghitung Hasil dari Atribut Member dan Non-Member</b>
-              </p>
-              <p>
-                P ( Kategori = Member) * P ( Y = Member ) = 0 P ( Kategori =
-                Non-Member) * P ( Y = Non-Member ) = 0.02215
-              </p>
-            </div>
-            <div>
-              <p>
-                <b>
-                  Kesimpulan hasil yang diperoleh dari perhitungan menunjukan
-                  bahwa masukan dari Data Test menghasilkan (P = Non-Member)
-                  dengan hasil probabilitas tertinggi.
-                </b>
-              </p>
-            </div>
+            {/* <div>
+              <img src="./gmb/1.png" alt="rumus" />
+              <img src="./gmb/2.png" alt="rumus" />
+            </div> */}
+
+            <table style={{ border: "1px solid black" }}>
+              <thead>
+                <tr style={{ border: "1px solid black" }}>
+                  <th style={{ border: "1px solid black" }}>Node</th>
+                  <th style={{ border: "1px solid black" }} />
+                  <th style={{ border: "1px solid black" }} />
+                  <th style={{ border: "1px solid black" }}>Jumlah</th>
+                  <th style={{ border: "1px solid black" }}>
+                    Lulus Tepat Waktu
+                  </th>
+                  <th style={{ border: "1px solid black" }}>
+                    Tidak Lulus Tepat Waktu
+                  </th>
+                  <th style={{ border: "1px solid black" }}>Entrophy</th>
+                  <th style={{ border: "1px solid black" }}>Gain</th>
+                </tr>
+              </thead>
+              <tbody style={{ border: "1px solid black" }}>
+                <tr style={{ border: "1px solid black" }}>
+                  <td style={{ border: "1px solid black" }} />
+                  <td style={{ border: "1px solid black" }}>Total</td>
+                  <td style={{ border: "1px solid black" }} />
+                  <td style={{ border: "1px solid black" }}>7</td>
+                  <td style={{ border: "1px solid black" }}>4</td>
+                  <td style={{ border: "1px solid black" }}>3</td>
+                  <td style={{ border: "1px solid black" }} />
+                  <td style={{ border: "1px solid black" }} />
+                </tr>
+                <tr>
+                  <td>IPK</td>
+                  <td>
+                    <tr>
+                      <td>.. </td>
+                    </tr>
+                    <tr>
+                      <td>Tinggi</td>
+                    </tr>
+                    <tr>
+                      <td>Sedang </td>
+                    </tr>
+                    <tr>
+                      <td> Rendah</td>
+                    </tr>
+                  </td>
+                  <td>
+                    <tr>
+                      <td>.. </td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                    </tr>
+                    <tr>
+                      <td>3 </td>
+                    </tr>
+                    <tr>
+                      <td> 3</td>
+                    </tr>
+                  </td>
+                  <td>
+                    <tr>
+                      <td>.. </td>
+                    </tr>
+                    <tr>
+                      <td>0</td>
+                    </tr>
+                    <tr>
+                      <td>2 </td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                    </tr>
+                  </td>
+                  <td>
+                    <tr>
+                      <td>.. </td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                    </tr>
+                    <tr>
+                      <td>1 </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                    </tr>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
